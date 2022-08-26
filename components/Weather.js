63d030667493f2d57c4b32b5fdaae5b44f247375
SahingkarnSuksabai <6310110471@psu.ac.th>
2022-08-27 00:57:40 +0700

    PART4 Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ImageBackground, Text, StyleSheet } from 'react-native';
 import { View } from "react-native";
 import Forecast from "./Forecast";
@@ -10,6 +10,25 @@ export default function Weather(props){
         description: '',
         temp: 0
     })
+
+    useEffect(() => {
+         console.log(`fetching data with zipCode = ${props.zipCode}`)
+          if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=426c4729ad15a988d0db1364cca1bac8`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                });
+            })
+            .catch((error) => {
+                console.warn(error);
+            });
+        }
+    }, [props.zipCode])
+
 
     return (
         <ImageBackground source={require('../bg.jpg')} style={style.backdrop}>
