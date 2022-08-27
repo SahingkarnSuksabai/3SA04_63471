63c5fcb86227a1eda288c32000aaa487f9d81cd9
SahingkarnSuksabai <6310110471@psu.ac.th>
2022-08-28 00:22:09 +0700

    PART6 Change color Headerbar, Edit Title

--- App.js
+++ App.js
@@ -10,8 +10,35 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name = "Home" component={ZipCodeScreen}/>
-        <Stack.Screen name = "Weather" component={WeatherScreen}/>
+      <Stack.Screen
+          name="Home"
+          component={ZipCodeScreen}
+          options={{
+          title: 'Home "Province"',
+          headerStyle: {
+            backgroundColor: 'rgba( 222, 184, 135, 1 )',
+          },
+          headerTintColor: '#660000',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+        <Stack.Screen
+          name="Weather"
+          component={WeatherScreen}
+          options={{
+          title: 'Weather',
+          headerStyle: {
+            backgroundColor: 'rgba( 143, 188, 143, 1 )',
+          },
+          headerTintColor: '#006666',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+
       </Stack.Navigator>
     </NavigationContainer>   
   );
